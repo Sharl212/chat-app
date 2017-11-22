e2e3a2d1b480ba2bbfa1f3a8a583e953f5cdc18d
Sharl <ssharl@ymail.com>
2017-11-22 03:59:43 +0200

    chat-app v0.1

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(publicPath))
@@ -8,6 +9,6 @@ app.get('/', function (req, res, next){
   res.render('index.html')
 });
 
-app.listen(3000,
+app.listen(port,
   console.log('server is up on localhost:3000')
 );
