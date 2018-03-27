0acad8e2e609ce1a01b2cc21e77b52bd6a945528
sharl212 <ssharl@ymail.com>
2018-03-27 13:15:58 +0200

    intial commit

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ const socketiO = require('socket.io'),
       io       = socketiO(server);
 
 // view engine setup
-// app.engine('html', cons.swig);
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'html');
+app.engine('html', cons.swig);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
